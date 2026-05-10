50e6f5b886abf893548e2831b28c7d3394217eaa
hannechi mohamed salah <mohamedhannachi047@gmail.com>
2026-05-10 21:55:35 +0100

    modif dans user entity et form

--- src/Form/User1Type.php
+++ src/Form/User1Type.php
@@ -6,14 +6,25 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 class User1Type extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('email')
-            ->add('roles')
+            ->add('roles', ChoiceType::class, [
+            'choices' => [
+            'Etudiant'    => 'ROLE_ETUDIANT',
+            'Responsable' => 'ROLE_RESPONSABLE',
+            'President'   => 'ROLE_PRESIDENT',
+            'Admin'       => 'ROLE_ADMIN',
+             ],
+            'multiple' => true,
+            'expanded' => true,
+            'label'    => 'Role'
+             ])
+
             ->add('password')
             ->add('nom')
             ->add('prenom')
