4328eb28ce0ea26ef6b5c8d9590677fce6d9519a
Mohamed Ben Abdelbaki <abdelbaki069@gmail.com>
2026-05-12 23:51:41 +0100

    Fixed upload image issue on Event page

--- src/Controller/EvenementController.php
+++ src/Controller/EvenementController.php
@@ -3,13 +3,16 @@
 namespace App\Controller;
 
 use App\Entity\Evenement;
+use App\Entity\Participation;
 use App\Form\EvenementType;
 use App\Repository\EvenementRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 #[Route('/evenement')]
 final class EvenementController extends AbstractController
@@ -23,13 +26,32 @@ final class EvenementController extends AbstractController
     }
 
     #[Route('/new', name: 'app_evenement_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    public function new(Request $request, EntityManagerInterface $entityManager, SluggerInterface $slugger): Response
     {
         $evenement = new Evenement();
         $form = $this->createForm(EvenementType::class, $evenement);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $imageFile = $form->get('image')->getData();
+
+            if ($imageFile) {
+                $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename . '-' . uniqid() . '.' . $imageFile->guessExtension();
+
+                try {
+                    $imageFile->move(
+                        $this->getParameter('evenements_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    $this->addFlash('error', 'Erreur lors de l\'upload de l\'image.');
+                }
+
+                $evenement->setImage($newFilename);
+            }
+
             $entityManager->persist($evenement);
             $entityManager->flush();
 
@@ -51,12 +73,31 @@ final class EvenementController extends AbstractController
     }
 
     #[Route('/{id}/edit', name: 'app_evenement_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Evenement $evenement, EntityManagerInterface $entityManager): Response
+    public function edit(Request $request, Evenement $evenement, EntityManagerInterface $entityManager, SluggerInterface $slugger): Response
     {
         $form = $this->createForm(EvenementType::class, $evenement);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $imageFile = $form->get('image')->getData();
+
+            if ($imageFile) {
+                $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename . '-' . uniqid() . '.' . $imageFile->guessExtension();
+
+                try {
+                    $imageFile->move(
+                        $this->getParameter('evenements_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    $this->addFlash('error', 'Erreur lors de l\'upload de l\'image.');
+                }
+
+                $evenement->setImage($newFilename);
+            }
+
             $entityManager->flush();
 
             return $this->redirectToRoute('app_evenement_index', [], Response::HTTP_SEE_OTHER);
@@ -66,6 +107,38 @@ final class EvenementController extends AbstractController
             'evenement' => $evenement,
             'form' => $form,
         ]);
+    }
+
+    #[Route('/{id}/participer', name: 'app_evenement_participate', methods: ['POST'])]
+    public function participate(Request $request, Evenement $evenement, EntityManagerInterface $entityManager): Response
+    {
+        $user = $this->getUser();
+        if (!$user) {
+            $this->addFlash('error', 'Vous devez être connecté pour participer.');
+            return $this->redirectToRoute('app_login');
+        }
+
+        $existing = $entityManager->getRepository(Participation::class)->findOneBy([
+            'user' => $user,
+            'evenement' => $evenement,
+        ]);
+
+        if ($existing) {
+            $this->addFlash('warning', 'Vous participez déjà à cet événement.');
+        } else {
+            $participation = new Participation();
+            $participation->setUser($user);
+            $participation->setEvenement($evenement);
+            $participation->setRegisteredAt(new \DateTimeImmutable());
+            $participation->setPresenceStatus('inscrit');
+
+            $entityManager->persist($participation);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Vous êtes inscrit à l\'événement !');
+        }
+
+        return $this->redirectToRoute('app_evenement_index');
     }
 
     #[Route('/{id}', name: 'app_evenement_delete', methods: ['POST'])]
